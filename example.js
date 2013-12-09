158cb52161ce336be8773598e77dd08af51b05b1
Jonathan Creasy <jonathan@ghostlab.net>
2013-12-09 14:44:20 -0600

    removed reference to UberAuth and console.log, added failed. event emission

--- example.js
+++ example.js
@@ -2,19 +2,39 @@ UberAuth = new Array();
 UberAuth['username'] = 'username';
 UberAuth['password'] = 'password';
 UberAuth['url'] = 'https://ubersmith.appliance/api/2.0';
-UberAuth['refresh.interval'] = 1;
 
 var ubersmith = require('ubersmith');
+// Set the Ubersmith Username, password and URL locations
 ubersmith.uberAuth = UberAuth;
+
+// fetch the device.list right now
 ubersmith.uberRefreshData('device.list');
-ubersmith.uberScheduleRefresh('device.list', UberAuth['refresh.interval']);
+
+//fetch the device list every 1 minute
+ubersmith.uberScheduleRefresh('device.list', 1);
 
 ubersmith.on('ready.device.list',
   function(body) {
     console.log(body);
   }
 );
+
 ubersmith.on('failed.device.list',
+  function(err) {
+    console.log(err);
+  }
+);
+
+ubersmith.uberRefreshData('client.list');
+ubersmith.uberScheduleRefresh('client.list', 10);
+
+ubersmith.on('ready.client.list',
+  function(body) {
+    console.log(body);
+  }
+);
+
+ubersmith.on('failed.client.list',
   function(err) {
     console.log(err);
   }
